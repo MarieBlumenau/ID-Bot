a530b8ee3148a5f48e2d765f1efbd6a377d91b9b
Marie Blumenau <marieblumenau@MBP-von-Marie.zyxel.box>
2023-11-18 20:06:14 +0100

    Repository hinzufügen

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import os
+import os.path
 import openai
 import gradio as gr
-from llama_index import StorageContext, load_index_from_storage
+from llama_index import (
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    StorageContext,
+    load_index_from_storage,
+)
 
 from theme import CustomTheme
 
+def initialize():
+    if not os.path.exists("./storage"):
+        # load the documents and create the index
+        documents = SimpleDirectoryReader("data").load_data()
+        index = VectorStoreIndex.from_documents(documents)
+        # store it for later
+        index.storage_context.persist()
 
 def response(message, history):
-    # rebuild storage context
-    storage_context = StorageContext.from_defaults(persist_dir="modulhandbuch")
-    # load index
+    storage_context = StorageContext.from_defaults(persist_dir="./storage")
     index = load_index_from_storage(storage_context)
 
     query_engine = index.as_query_engine()
@@ -20,6 +31,7 @@ def response(message, history):
 
 def main():
     openai.api_key = os.environ["OPENAI_API_KEY"]
+    initialize()
 
     custom_theme = CustomTheme()
 
